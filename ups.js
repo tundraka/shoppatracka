a999494e013f0b1da74e3df37cde04170fa715a2
tundraka <tundraka@gmail.com>
2016-03-04 23:41:54 -0600

    Trying to read from UPS.

---
+++ ups.js
@@ -0,0 +1,20 @@
+var upsApi = require('shipping-ups')({
+    environment: 'live',
+    access_key: process.env.upsAccessKey,
+    username: process.env.upsUserName,
+    password: process.env.upsPassword,
+    user_agent: 'shoppatracka 0.1'
+});
+
+upsApi.track('', (err, result) => {
+    if (err) {
+        console.log('error ' + err);
+        console.log(JSON.stringify(err));
+        return;
+    }
+
+    return {
+    };
+});
+
+//module.exports = upsApi;

5d965d35342a642d96b90b80b47416ab94c11408
tundraka <tundraka@gmail.com>
2016-03-14 23:36:51 -0500

    Reading variables

--- ups.js
+++ ups.js
@@ -14,17 +14,17 @@ var upsApi = new UpsApi({
 Promise.promisifyAll(upsApi);
 
 function trackPackage(trackingNumber) {
-    return upsApi.trackAsync('1Z5466360394880552o', {latest: true}).then((result) => {
-        console.log(JSON.stringify(result));
+    return upsApi.trackAsync(trackingNumber, {latest: true}).then((result) => {
         // TODO. I'm not considering the case when the tracking code doesn't
         // exist.
         let activity = result.Shipment.Package.Activity;
         if (Array.isArray(activity) && activity.length > 0) {
-            activity.date || '';
-            activity.time || '';
+            let date = activity.date || '';
+            let time = activity.time || '';
+            let place = activity.ActivityLocation.Address.City || '';
+            let statusCode = activity.Status.StatusType.Code || '';
+            let statusDescription = activity.Status.StatusType.Description || '';
         }
-    }).catch((e) => {
-        console.log(JSON.stringify(e));
     });
 }
 
@@ -32,7 +32,7 @@ module.exports = {
     trackPackage
 };
 
-trackPackage().then(() => {
+trackPackage('1Z5466360394880552').then(() => {
     console.log('hey');
 }).catch((err) => {
     console.log('hey');

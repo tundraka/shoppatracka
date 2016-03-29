4cefee4ee81d2e8d7229103a5ab72dbd09a988c2
tundraka <tundraka@gmail.com>
2016-03-28 22:30:48 -0500

    typos

--- shoppatracka.js
+++ shoppatracka.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const shopatracka = require('facades/botkit');
-const commands = require('commands/manager');
+const shopatracka = require('./facades/botkit');
+const commands = require('./commands/manager');
 
 commands.init(shoppatracka);

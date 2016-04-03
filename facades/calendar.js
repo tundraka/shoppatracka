2ab4c2e98531ae5383a65e1ba82efaaaad70cf5a
tundraka <tundraka@gmail.com>
2016-04-02 23:52:08 -0500

    more google cal

--- facades/calendar.js
+++ facades/calendar.js
@@ -1,20 +1,14 @@
 'use strict';
 
-const passport = require('passport');
+const constants = require('../utils/constants');
+const clientToken = require(constants.google.tokens);
+const secret = require(constats.google.tokens);
+const oauth2Client = new auth.OAuth2(secret.installed.clientId,
+                                     secret.installed.clientSecret,
+                                     secret.installed.redirectUrl);
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const clientSecret = require(process.env.google);
 
-passport.use(new GoogleStrategy({
-    clientID: clientSecret.web.client_id,
-    clientSecret: clientSecret.web.client_secret,
-    callbackURL: 'http://tundraka.com',
-    scope: ['https://www.googleapis.com/auth/calendar']
-}, (accessToken, refreshToken, profile, done) => {
-    console.log(JSON.stringify(accessToken));
-    console.log(JSON.stringify(refreshToken));
-    console.log(JSON.stringify(profile));
-
-    return done(null, null);
-}));
+function getToken() {
+}
 
 passport.authenticate('google');
---
+++ utils/constants.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const constants = {
+    google: {
+        tokens: '',
+        secret: ''
+    }
+};
+
+module.exports = constants;

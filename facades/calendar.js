b8f239d5b02061103760eb51f23e5b385a3a0435
tundraka <tundraka@gmail.com>
2016-04-03 23:53:40 -0500

    more attempts

--- facades/calendar.js
+++ facades/calendar.js
@@ -1,14 +1,31 @@
 'use strict';
 
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'));
 const constants = require('../utils/constants');
 const clientToken = require(constants.google.tokens);
 const secret = require(constats.google.tokens);
 const oauth2Client = new auth.OAuth2(secret.installed.clientId,
                                      secret.installed.clientSecret,
                                      secret.installed.redirectUrl);
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+function getSecret() {
+    let creds = {};
+
+    fs.readFileAsync(constants.google.secret).
+        then((content) => {
+        creds.secret = JSON.parse(content);
+    });
+}
 
 function getToken() {
 }
 
-passport.authenticate('google');
+function getEvents() {
+    let secret = getSecret();
+    let token = getToken();
+}
+
+module.exports = {
+    getEvents
+};
--- utils/constants.js
+++ utils/constants.js
@@ -2,8 +2,12 @@
 
 const constants = {
     google: {
-        tokens: '',
-        secret: ''
+        tokens: process.env.google.token,
+        secret: process.env.google.secret,
+        calendar: {
+            calendarid: process.env.google.calendarid,
+            scope: ['https://www.googleapis.com/auth/calendar.readonly']
+        }
     }
 };
 

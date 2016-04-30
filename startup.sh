804348d49a2f984c4279daee2ab09affc1cd53fd
tundraka <tundraka@gmail.com>
2016-04-30 11:12:36 -0500

    removing nconf

--- startup.sh
+++ startup.sh
@@ -1,14 +1,14 @@
-PROJECT=shoppatracka
-cd code/$PROJECT
-pm2 stop $PROJECT
-git pull
-
+# Google
 google_token='path/to/file' \
-    google_secret='path/to/file' \
-    google_calendarid='cal@id.id' \
-    slack_token=token \
-    forecastiokey=token \
-    upsAccessKey=token \
-    upsUserName=username \
-    upsPassword='token' \
-pm2 start ${PROJECT}.js
+google_secret='path/to/file' \
+google_calendarid='cal@id.id' \
+
+# UPS
+upsAccessKey=token \
+upsUserName=username \
+upsPassword='token' \
+
+# Other
+slack_token=token \
+forecastiokey=token \
+pm2 start shoppatracka.js

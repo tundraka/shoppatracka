de704ce5b64f78d604ad494ec9adfdb90b7f323c
tundraka <tundraka@gmail.com>
2016-05-07 22:41:32 -0500

    bot say?

--- tasks/manager.js
+++ tasks/manager.js
@@ -6,15 +6,8 @@ const moment = require('moment');
 
 function init(bot) {
     schedule.scheduleJob('* * */1 * * *', () => {
-        bot.startPrivateConversation({user: constants.get('slack.user')}, (err, convo) => {
-            if (err) {
-                // TODO logs?
-                return;
-            }
-
-            let now = moment().format();
-            convo.say(`I am here, it's ${now}`);
-        });
+        let now = moment().format();
+        bot.say(`I am here, it's ${now}`);
     });
 }
 

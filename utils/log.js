b200d328515a0ffe44568bbf109893cf38360ed0
tundraka <tundraka@gmail.com>
2016-05-15 22:58:51 -0500

    unused code

--- utils/log.js
+++ utils/log.js
@@ -7,16 +7,6 @@ const logHome = constants.get('globals.loghome')
 winston.add(winston.transports.File, {filename: `${logHome}/stdout.log`});
 winston.remove(winston.transports.Console);
 
-const defaultConf = {
-    file: {
-        level: 'info',
-        label: 'stdout',
-        file: {
-            filename: `${logHome}/stdout.log`
-        }
-    }
-};
-
 function getLog(loggerName) {
     return (() => {
         let log = winston.loggers.get(loggerName);

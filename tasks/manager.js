34172c8ed57d562dbc487dfb6fc0fb4957e23706
tundraka <tundraka@gmail.com>
2016-05-29 22:40:06 -0500

    Gollum~~~~~!!1111

--- tasks/manager.js
+++ tasks/manager.js
@@ -1,13 +1,15 @@
 'use strict';
 
 const schedule = require('node-schedule');
-//const moment = require('moment');
+const moment = require('moment');
 const log = require('../utils/log').getLog('task.manager');
 const baseHook = require('../hooks/base');
 
 function init() {
     schedule.scheduleJob('0 0 * * * *', () => {
-        log.info('I am here.');
+        let now = moment().format();
+
+        log.info(`I am here ${now}`);
         baseHook.sendHook();
     });
 }

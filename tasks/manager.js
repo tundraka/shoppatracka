22e32ddb2bc42f0d2cdcaee4945f3f32116e3f94
tundraka <tundraka@gmail.com>
2016-06-03 23:18:08 -0500

    5am

--- tasks/manager.js
+++ tasks/manager.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const schedule = require('node-crontab');
+const schedule = require('node-schedule');
 const moment = require('moment');
 const log = require('../utils/log').getLog('task.manager');
 const baseHook = require('../hooks/base');
 
 function init() {
-    schedule.scheduleJob('* * 13 * *', () => {
+    schedule.scheduleJob({hour: 5, minute: 0}, () => {
         let now = moment().format();
 
         log.info(`I am here ${now}`);

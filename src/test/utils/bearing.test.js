1667c12bef0f8df4de72c39dfea3032e5955d86a
tundraka <tundraka@gmail.com>
2016-06-14 22:32:17 -0500

    eslint for jasmine.

--- src/test/utils/bearing.test.js
+++ src/test/utils/bearing.test.js
@@ -2,18 +2,23 @@
 
 let bearing = require('../../main/utils/bearing');
 
+const lowerRange = -1;
+const upperRange = 361;
+const north = 360;
+const east = 90;
+
 describe('bearing', () => {
     it('doesn\'t know about a bearing smaller than 0', () => {
-        expect(bearing(-1)).toBe('I don\'t know about bearing -1.');
+        expect(bearing(lowerRange)).toBe('I don\'t know about bearing -1.');
     });
 
     it('doesn\'t know about a bearing greater than 360', () => {
-        expect(bearing(361)).toBe('I don\'t know about bearing 361.');
+        expect(bearing(upperRange)).toBe('I don\'t know about bearing 361.');
     });
 
     it('provides a bearing for a valid angle', () => {
         expect(bearing(0)).toBe('North');
-        expect(bearing(360)).toBe('North');
-        expect(bearing(90)).toBe('East');
+        expect(bearing(north)).toBe('North');
+        expect(bearing(east)).toBe('East');
     });
 });

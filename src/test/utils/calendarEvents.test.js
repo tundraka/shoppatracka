a4654530460808b0a02649a482c6e2f517302fb6
tundraka <tundraka@gmail.com>
2016-06-17 22:46:56 -0500

    trying to spy on moment

--- src/test/utils/calendarEvents.test.js
+++ src/test/utils/calendarEvents.test.js
@@ -1,14 +1,21 @@
 'use strict';
 
 const calendarEvents = require('../../main/utils/calendarEvents');
-const moment = require('moment');
+const momento = require('moment');
 const calendar = require('../../main/facades/google/calendar');
 
 describe('calendarEvents', () => {
     it('calls calendar with next week dates', function() {
-       spyOn(calendar, 'getEvents');
-       calendarEvents.getNextWeekEvents();
+        var moment = jasmine.createSpy('moment');
 
-       expect(calendar.getEvents).toHaveBeenCalled();
+        //calendarEvents.prototype.moment = moment;
+        console.log(JSON.stringify(calendarEvents));
+        moment.and.callFake(() => {});
+        spyOn(calendar, 'getEvents');
+        calendarEvents.getNextWeekEvents();
+        var a = new Date('Sat Jun 25 2016 00:00:00 GMT-0500');
+        var b = new Date('Fri Jun 24 2016 22:36:18 GMT-0500');
+
+        expect(calendar.getEvents).toHaveBeenCalledWith(a, b);
     });
 });

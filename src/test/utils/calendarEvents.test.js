f56dae49167da53f5e91607f419faefd7f4ed26a
tundraka <tundraka@gmail.com>
2016-06-19 16:58:51 -0500

    more tests.

--- src/test/utils/calendarEvents.test.js
+++ src/test/utils/calendarEvents.test.js
@@ -1,19 +1,39 @@
 'use strict';
 
 const calendarEvents = require('../../main/utils/calendarEvents');
-const momento = require('moment');
 const calendar = require('../../main/facades/google/calendar');
+const moment = require('moment');
 
 describe('calendarEvents', () => {
+    let getEventsSpy;
+
+    beforeEach(() => {
+        getEventsSpy = spyOn(calendar, 'getEvents');
+    });
+
     it('calls calendar with next week dates', function() {
-        spyOn(calendar, 'getEvents');
         calendarEvents.getNextWeekEvents();
 
-        var dates = calendar.getEvents.calls.argsFor(0);
-        var a = dates[0];
-        var b = dates[1];
+        var dates = getEventsSpy.calls.argsFor(0);
+        var start = dates[0];
+        var end = dates[1];
         
-        expect(a).not.toEqual(b);
-        expect(b.diff(a, 'week')).toBe(1);
+        expect(start).not.toEqual(end);
+        expect(end.diff(start, 'week')).toBe(1);
+    });
+
+    it('calls calendar with today\'s date', () => {
+        calendarEvents.getTodaysEvents();
+
+        let dateParam = getEventsSpy.calls.argsFor(0);
+        let start = dateParam[0];
+        let end = dateParam[1];
+        let today = moment().day();
+
+        expect(start).not.toBeNull();
+        expect(start.day()).toEqual(today);
+        expect(end.day()).toEqual(today);
+        expect(end.hour()).toBe(23);
+        expect(end.minute()).toBe(59);
     });
 });

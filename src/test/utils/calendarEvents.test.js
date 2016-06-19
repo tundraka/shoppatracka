b9d4c1895c3073a4e8c94b572acef36f33aa3d80
tundraka <tundraka@gmail.com>
2016-06-18 23:57:41 -0500

    test

--- src/test/utils/calendarEvents.test.js
+++ src/test/utils/calendarEvents.test.js
@@ -6,16 +6,14 @@ const calendar = require('../../main/facades/google/calendar');
 
 describe('calendarEvents', () => {
     it('calls calendar with next week dates', function() {
-        var moment = jasmine.createSpy('moment');
-
-        //calendarEvents.prototype.moment = moment;
-        console.log(JSON.stringify(calendarEvents));
-        moment.and.callFake(() => {});
         spyOn(calendar, 'getEvents');
         calendarEvents.getNextWeekEvents();
-        var a = new Date('Sat Jun 25 2016 00:00:00 GMT-0500');
-        var b = new Date('Fri Jun 24 2016 22:36:18 GMT-0500');
 
-        expect(calendar.getEvents).toHaveBeenCalledWith(a, b);
+        var dates = calendar.getEvents.calls.argsFor(0);
+        var a = dates[0];
+        var b = dates[1];
+        
+        expect(a).not.toEqual(b);
+        //expect(b.diff(a, 'weeks')).toBe(1);
     });
 });
